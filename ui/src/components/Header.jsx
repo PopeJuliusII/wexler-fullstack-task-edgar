250a425a802ae57652df8b06b1fab3319234c2f9
Pope Julius II <PopeJuliusII@PapalStates>
2024-10-27 22:34:05 +0000

    Created a basic header for the page.

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import logo from "../assets/images/logo-green-tld.svg";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <div className="navbar flex justify-center items-center h-16">
+      <a
+        className="btn btn-ghost text-xl inline-flex items-center"
+        href="https://www.wexler.ai"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={logo} alt="Logo" />
+      </a>
+    </div>
+  );
 };
 
 export default Header;
