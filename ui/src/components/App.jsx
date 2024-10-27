d645858442f0dc46d943a880704b7ef015d10b89
Pope Julius II <PopeJuliusII@PapalStates>
2024-10-27 15:33:26 +0000

    Moved the App.js file into a new components folder, and created boilerplate files for the Header and Footer.

---
+++ ui/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div>Image Management System</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
