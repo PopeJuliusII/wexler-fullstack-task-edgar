041683ffad1b390d46449daab1e7a671a4b9a915
Pope Julius II <PopeJuliusII@PapalStates>
2024-10-28 09:20:23 +0000

    Added api-related constants to the config file.

--- ui/src/config/constants.js
+++ ui/src/config/constants.js
@@ -5,9 +5,10 @@
 // Upload image constants - constants used when determining
 // the size and nature of the images to upload.
 export const MAX_FILE_COUNT = 20;
-export const MAX_FILE_SIZE = 100 * 1024 * 1024; // 100 MB
+export const MAX_FILE_SIZE = 80 * 1024 * 1024; // MB
 export const NEW_NAME_MAX_LENGTH = 20;
 export const NEW_NAME_REGEX = /^[a-zA-Z0-9]{1,20}$/;
 
 // Upload image endpoints - endpoints used when uploading images.
-export const UPLOAD_ENDPOINT_BASE = "http://localhost:9001";
+export const API_ENDPOINT_BASE = "http://localhost:9001";
+export const IMAGES_ENDPOINT_SUFFIX = "/images";
