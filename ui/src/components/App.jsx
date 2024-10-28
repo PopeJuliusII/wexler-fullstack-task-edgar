0c4d137aa20883192c2838f49aec78f51938a502
Pope Julius II <PopeJuliusII@PapalStates>
2024-10-28 20:02:20 +0000

    Incorporated the ImageGrid into the App.jsx file.

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import Header from "./Header";
 import UploadConsole from "./UploadConsole";
+import ImageGrid from "./ImageGrid";
 
 function App() {
+
+  //TODO: A significant gain in performance can be achieved by only
+  // pulling the specific number of images needed. This could be achieved
+  // by using a paginated endpoint and converting recompute to a number of images.
+  const [recompute, setRecompute] = useState(true);
+
+  const handleUploadSuccess = useCallback(() => {
+    setRecompute((prev) => !prev);
+  }, []);
+
   return (
-    <div className="bg-base-200">
+    <div className="bg-base-200  min-h-screen flex flex-col">
       <Header />
-      <UploadConsole />
+      <UploadConsole onUploadSuccess={handleUploadSuccess} />
+      <ImageGrid recompute={recompute} setRecompute={setRecompute} />
     </div>
   );
 }
