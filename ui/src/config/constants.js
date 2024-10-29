5ae4f20c8fa27a1e820a2e9a382a5150adda57e6
Pope Julius II <PopeJuliusII@PapalStates>
2024-10-29 11:44:35 +0000

    Lowered the maximum allowable file size to 10% below Imgur's limit for some breathing room.

--- ui/src/config/constants.js
+++ ui/src/config/constants.js
@@ -8,7 +8,7 @@ export const MAX_FILE_COUNT = 20;
 
 // 20 MB is the Imgur limit for non-animated images.
 // https://help.imgur.com/hc/en-us/articles/26511665959579-What-files-can-I-upload-Is-there-a-size-limit
-export const MAX_FILE_SIZE = 20 * 1024 * 1024; // MB
+export const MAX_FILE_SIZE = 18 * 1024 * 1024; // MB
 export const NEW_NAME_MAX_LENGTH = 20;
 export const NEW_NAME_REGEX = /^[a-zA-Z0-9]{1,20}$/;
 
